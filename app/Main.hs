8cbb8ded5e041abc2c9debad58190716ee8201d4
Honore Dupieu <honore.dupieu@epitech.eu>
2021-11-19 17:34:52 +0100

    [ADD] Tree evaluation and builtins

--- app/Main.hs
+++ app/Main.hs
@@ -16,6 +16,9 @@ import CLIArguments.Parser  ( parseArgs, HalExecution(..) )
 import CLIArguments.Error   ( Error(..) )
 import Interpreter.Lexer    -- ( buildEvaluationTree, HalExecution(..) )
 import Interpreter.Parser   -- ( buildEvaluationTree, HalExecution(..) )
+import Interpreter.Evaluate (EvaluatedValue(..), EvaluatingContext(..), EvaluationResult(..), Register(..), evaluateTree)
+import Interpreter.Builtins (initialRegister)
+import System.IO (hFlush, stdout)
 -- import Error                ( Error(..) )
 
 main :: IO ()
@@ -25,31 +28,47 @@ main = catches
         -- , Handler handleErrors
         ]
 
-type Context = Int
+-- type Context = Int
 
 dispatchExecutions :: HalExecution -> IO ()
-dispatchExecutions execution = dispatchExecutions' execution emptyContext
+dispatchExecutions execution = dispatchExecutions' execution initialRegister
 
-dispatchExecutions' :: HalExecution -> Context -> IO ()
+dispatchExecutions' :: HalExecution -> Register -> IO ()
 dispatchExecutions' PrintHelp               _       = printHelp
 dispatchExecutions' (Evaluate []          ) _       = throw $ ArgumentParsingError "Nothing to evaluate."
-dispatchExecutions' (Evaluate [file]      ) context = readFile file >>= \content -> print $ fst $ evaluateExpr context content
-dispatchExecutions' (Evaluate (file : xs) ) context = readFile file >>= \content -> dispatchExecutions' (Evaluate xs) $ snd $ evaluateExpr context content
-dispatchExecutions' (Repl     []          ) context = infiniteLoop context
-dispatchExecutions' (Repl     (file : xs) ) context = readFile file >>= \content -> dispatchExecutions' (Repl xs) $ snd $ evaluateExpr context content
+dispatchExecutions' (Evaluate [file]      ) reg = readFile file >>= \content -> print $ resVal $ evaluateFile reg content
+dispatchExecutions' (Evaluate (file : xs) ) reg = readFile file >>= \content -> dispatchExecutions' (Evaluate xs) $ resReg $ evaluateFile reg content
+dispatchExecutions' (Repl     []          ) reg = infiniteLoop reg
+dispatchExecutions' (Repl     (file : xs) ) reg = readFile file >>= \content -> dispatchExecutions' (Repl xs) $ resReg $ evaluateFile reg content
 
 printHelp :: IO ()
 printHelp = putStrLn "This is a help."
 
-emptyContext :: Context
-emptyContext = 2
+-- emptyContext :: EvaluatingContext
+-- emptyContext = Context (initialRegister, Empty)
+-- (cond (#t 42))
+infiniteLoop :: Register -> IO ()
+infiniteLoop reg = putStr "> " >> hFlush stdout >> (getLine >>= \line -> infiniteLoop' reg line)
 
-infiniteLoop :: Context -> IO ()
-infiniteLoop _ = putStrLn "Infinite loop..."
+infiniteLoop' :: Register -> String -> IO ()
+infiniteLoop' reg line = let (Result (newReg, res)) = evaluateFile reg line in print res >> infiniteLoop newReg
 
-type EvaluatedValue = [Tree]
-evaluateExpr :: Context -> String -> (EvaluatedValue, Context)
-evaluateExpr _ str = (buildExpressionsTrees $ tokenize str, 2)
+-- type EvaluatedValue = [Tree]
+evaluateFile :: Register -> String -> EvaluationResult
+evaluateFile reg str = evaluateTrees reg $ buildExpressionsTrees $ tokenize str
+
+evaluateTrees :: Register -> [Tree] -> EvaluationResult
+evaluateTrees reg = evaluateTrees' (Result (reg, NoValue))
+
+evaluateTrees' :: EvaluationResult -> [Tree] -> EvaluationResult
+evaluateTrees' res               []               = res
+evaluateTrees' (Result (reg, _)) (tree : others)  = evaluateTrees' (evaluateTree (Context (reg, tree))) others
+
+resReg :: EvaluationResult -> Register
+resReg (Result (reg, _)) = reg
+
+resVal :: EvaluationResult -> EvaluatedValue
+resVal (Result (_, val)) = val
 
 handleCLIArgumentsErrors :: CLIArguments.Error.Error -> IO ()
 handleCLIArgumentsErrors (InvalidOption optionName) = putStrLn ("Invalid option '" ++ optionName ++ "'")  >> exitWith (ExitFailure 84)
